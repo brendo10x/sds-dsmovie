960ddda420ffbeeb8ed1a26c5f96099827f39891
Brendo Arcanjo <brendo10x@gmail.com>
2022-01-15 11:11:17 -0300

    Ajustando imports

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,5 @@
-import {Link, useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import '../../components/FormCard/styles.css';
-import {Movie} from "../../types/movie";
 import FormCard from "../../components/FormCard";
 
 function Form() {
